7638aea96b81751b8d8c2dd4a5a66696831d6bfc
Yair lcpa <yairlcpa@outlook.com>
2024-03-30 15:17:55 +0300

    update-sw

--- sw.js
+++ sw.js
@@ -2,10 +2,10 @@
 self.addEventListener("install", e => {
   e.waitUntil(
     caches.open("static").then(cache => {return cache.addAll([
-      "./", "./index.js", "./main.js", "./style.css",
+      "./", "./index.html", "./main.js", "./style.css",
       "./src/pdf-lib.js", "./src/bootstrap.css", 
       "./src/logo.png", "./src/logo144.png", "./src/logo192.png", "./src/logo512.png",
-      "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css",
+      "https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"
       // "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
     ]);
     })
